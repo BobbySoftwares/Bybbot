aee5f47d895d6a72afafbdf65c6dfae48a00eb8c
gggto <47183108+gggto@users.noreply.github.com>
2021-06-18 23:35:43 +0200

    Update cauchy.py

--- cauchy.py
+++ cauchy.py
@@ -4,13 +4,13 @@ from numpy import round, abs
 
 def roll(loc, scale):
     """Génère un entier aléatoire suivant une loi de Cauchy modifiée
-    à valeurs dans l'ensemble des entiers naturels
+    à valeurs dans l'ensemble des entiers naturels.
 
     Args:
-        loc (float): Centre de la distribution de probabilités
-        scale (float): Largeur de la distribution de probabilités
+        loc (float): Centre de la distribution de probabilités.
+        scale (float): Largeur de la distribution de probabilités.
 
     Returns:
-        int: Nombre aléatoire généré
+        int: Nombre aléatoire généré.
     """
     return int(abs(round(cauchy.rvs(loc, scale))))
