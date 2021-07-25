63830b4116e2c5f0019fa4a26296aa66cb8a3a66
GlitchiKun <charlie.poorun@univ-tlse3.fr>
2021-07-25 13:33:55 +0200

    Remove old import

--- swag/swag_client.py
+++ swag/swag_client.py
@@ -1,7 +1,6 @@
 from swag.errors import (
     CagnotteNameAlreadyExist,
     ForbiddenDestructionOfCagnotte,
-    NoCagnotteIdxInCommand,
     NoCagnotteRegistered,
     NotEnoughMoneyInCagnotte,
     NotInManagerGroupCagnotte,
