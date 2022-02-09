973c5c04f33759639d89bf8203313988ec1505ab
GlitchiKun <charlie.poorun@univ-tlse3.fr>
2022-02-09 23:22:16 +0100

    [WIP] add SelectOption creation in Interface Humain Swag (IHS)

--- swag/client/IHS_toolkit.py
+++ swag/client/IHS_toolkit.py
@@ -2,9 +2,12 @@ from typing import TYPE_CHECKING
 
 from disnake import SelectOption
 
+from utils import get_guild_member_name
+
 if TYPE_CHECKING:
     from swag.id import YfuId
     from disnake.ui import Select
+    from swag.artefacts.accounts import Info
 
 
 def sort_yfus_id(yfu_set):
@@ -20,4 +23,26 @@ def yfus_to_select_options(yfus):
             value=index,
         )
         for index, yfu in enumerate(yfus)
+    ]
+
+
+def forbes_to_select_options(forbes, guild, client):
+    return [
+        SelectOption(
+            label=get_guild_member_name(user_id, guild, client),
+            description=f"{account.swag_balance} {account.style_balance} {len(account.yfu_wallet)} ¥fu(s)",
+            value=index,
+        )
+        for index, user_id, account in enumerate(forbes)
+    ]
+
+
+def cagnottes_to_select_options(cagnottes):
+    return [
+        SelectOption(
+            label=f"{cagnotte_id} {cagnotte.name}",
+            description=f"{cagnotte.swag_balance} {cagnotte.style_balance} {len(cagnotte.yfu_wallet)} ¥fu(s)",
+            value=index,
+        )
+        for index, cagnotte_id, cagnotte in enumerate(cagnottes)
     ]
