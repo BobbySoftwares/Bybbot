282b55bcfa77dfd1402c1ece580bc481adddb5a3
GlitchiKun <p.charlie17@gmail.com>
2022-04-18 20:01:37 +0200

    Delete useless StyleGeneration Block in $wagchain Fixes #24
    Add the _messages member in the synch_chain to keep the link of discord message id and block in order to easily delete it.

--- swag/blockchain/synced_blockchain.py
+++ swag/blockchain/synced_blockchain.py
@@ -1,8 +1,10 @@
 import json
+from typing import Dict
 from attr import attrs, attrib
 from arrow import Arrow
 from disnake import TextChannel
 import disnake
+from swag.block import Block
 
 from swag.blocks.yfu_blocks import YfuGenerationBlock
 
@@ -19,15 +21,18 @@ def json_converter(o):
 class SyncedSwagChain(SwagChain):
     _id: int = attrib()
     _channel: TextChannel = attrib(init=False, default=None)
+    _messages: Dict[int,Block] = attrib(init=False, default={})
 
     @classmethod
     async def from_channel(cls, bot_id: int, channel: TextChannel):
         synced_chain = cls([], bot_id)
         synced_chain._channel = channel
+        synced_chain._messages = {}
         async for message in channel.history(limit=None, oldest_first=True):
             unstructured_block = json.loads(message.content)
             block = structure_block(unstructured_block)
             SwagChain.append(synced_chain, block)
+            synced_chain._messages[block] = message.id
             if isinstance(block, YfuGenerationBlock):
                 # Mise à jour de l'url de l'avatar dans la Yfu
                 avatar_url = message.attachments[0].url
@@ -40,17 +45,19 @@ class SyncedSwagChain(SwagChain):
 
         # Envoie de l'avatar si generation de Yfu
         if isinstance(block, YfuGenerationBlock):
-            avatar_message = await self._channel.send(
+            message = await self._channel.send(
                 json.dumps(unstructure_block(block), default=json_converter),
                 file=disnake.File(block.avatar_local_path),
             )
             # Mise à jour de l'url de l'avatar dans la Yfu
-            avatar_url = avatar_message.attachments[0].url
+            avatar_url = message.attachments[0].url
             self._yfus[block.yfu_id].avatar_url = avatar_url
         else:
-            await self._channel.send(
+            message = await self._channel.send(
                 json.dumps(unstructure_block(block), default=json_converter)
             )
+
+        self._messages[block] = message.id
         # try:
         #     self._chain.append(block)
         #     await self._channel.send(json.dumps(unstructure_block(block)))
@@ -60,3 +67,9 @@ class SyncedSwagChain(SwagChain):
         #         "bound to a TextChannel. Please use SyncedSwagChain.from_channel "
         #         "to create a bounded instance."
         #     )
+
+    async def remove(self, block):
+        SwagChain.remove(self,block)
+        print(f"Delation of {block}")
+        await self._channel.get_partial_message(self._messages.pop(block)).delete()
+        
