1a90138eede1ee64291d07260b44e463c0940141
GlitchiKun <p.charlie17@gmail.com>
2022-05-07 14:12:23 +0200

    Add log for /commands errors

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ COMMAND_CHANNEL_ID = client_config.get("command_channel", 848302082150760508)
 # ID unique du canal de la swag-chain
 SWAGCHAIN_CHANNEL_ID = client_config.get("swagchain_channel", 913946510616567848)
 
-# ID unique du canal de log, si il n'est pas défini, sa valeur sera False
+# ID unique du canal de log, si il n'est pas défini, sa valeur sera None
 LOG_CHANNEL_ID = client_config.get("log_channel", None)
 
 
